7ea81f0510495e1233eeaff934a0b91fb8ae4410
josephgakono <sirg4kono@gmail.com>
2026-05-13 12:51:44 +0300

    adjust api to focus on one movie title

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ import requests
 API_KEY = "aa15d8f5"
 
 def search_movie(movie_name):
-    url = f"http://www.omdbapi.com/?s={movie_name}&apikey={API_KEY}"
+    url = f"http://www.omdbapi.com/?t={movie_name}&apikey={API_KEY}"
 
     response = requests.get(url)
     data = response.json()
