e99f163e26cc2ce3f0768a618d6f92f73d7087ca
josephgakono <sirg4kono@gmail.com>
2026-05-18 12:03:32 +0300

    Add OMDb title search helper

--- api.py
+++ api.py
@@ -2,10 +2,25 @@ import requests
 
 API_KEY = "aa15d8f5"
 
-def search_movie(movie_name):
-    url = f"http://www.omdbapi.com/?t={movie_name}&apikey={API_KEY}"
 
-    response = requests.get(url)
+def search_movie(movie_name):
+    response = requests.get(
+        "http://www.omdbapi.com/",
+        params={"t": movie_name, "apikey": API_KEY},
+    )
     data = response.json()
 
     return data
+
+
+def search_movies(query):
+    response = requests.get(
+        "http://www.omdbapi.com/",
+        params={"s": query, "type": "movie", "apikey": API_KEY},
+    )
+    data = response.json()
+
+    if data.get("Response") != "True":
+        return []
+
+    return data.get("Search", [])
